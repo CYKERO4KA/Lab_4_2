f44391d1ea1ce835a98678b960ef50496eec8e68
CYKERO4KA <cykero4ka88@gmail.com>
2023-03-01 21:00:03 +0200

    First commit

---
+++ Lab_4_2/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Lab_4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    class Provider
+    {
+        
+    }
+
+    class Client
+    {
+        
+    }
+
+    class Tariff
+    {
+        
+    }
+}
